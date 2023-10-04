53331f5d0136685060970a6370b6292ff89f39a3
میثم سفیدکار <sefidkar.m@kwpa.gov.ir>
2023-10-04 11:46:25 +0330

    Init commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+
+// Add fixed class to navbar
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
+
+// Show sidebar
+
+navBtn.addEventListener("click", () => {
+  sidebar.classList.add("show-sidebar");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("show-sidebar");
+});
